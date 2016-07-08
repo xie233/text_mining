61e0d4111107c4a3b95a7325be0887444ba3d441
xie233 <x1311178474@gmail.com>
2016-07-08 12:10:06 +0800

    simple

---
+++ chat/Chat.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import re
+import random
+from nltk import compat
+
+reflections = {
+  "i am"       : "you are",
+  "i was"      : "you were",
+  "i"          : "you",
+  "i'm"        : "you are",
+  "i'd"        : "you would",
+  "i've"       : "you have",
+  "i'll"       : "you will",
+  "my"         : "your",
+  "you are"    : "I am",
+  "you were"   : "I was",
+  "you've"     : "I have",
+  "you'll"     : "I will",
+  "your"       : "my",
+  "yours"      : "mine",
+  "you"        : "me",
+  "me"         : "you"
+}
+
+class Chat(object):
+    def __init__(self, pairs, reflections={}):
+ 
+        self._pairs = [(re.compile(x, re.IGNORECASE),y) for (x,y) in pairs]
+        self._reflections = reflections
+        self._regex = self._compile_reflections()
+    def _compile_reflections(self):
+        sorted_refl = sorted(self._reflections.keys(), key=len,
+                reverse=True)
+        return  re.compile(r"\b({0})\b".format("|".join(map(re.escape,
+            sorted_refl))), re.IGNORECASE)
+    def _substitute(self, str):
+        return self._regex.sub(lambda mo:
+                self._reflections[mo.string[mo.start():mo.end()]],
+                    str.lower())
+    def _wildcards(self, response, match):
+        pos = response.find('%')
+        while pos >= 0:
+            num = int(response[pos+1:pos+2])
+            response = response[:pos] + \
+                self._substitute(match.group(num)) + \
+                response[pos+2:]
+            pos = response.find('%')
+        return response
+    def respond(self, str):
+        for (pattern, response) in self._pairs:
+            match = pattern.match(str)
+            if match:
+                resp = random.choice(response)  
+                resp = self._wildcards(resp, match) 
+                if resp[-2:] == '?.': resp = resp[:-2] + '.'
+                if resp[-2:] == '??': resp = resp[:-2] + '?'
+                return resp
+
+    def converse(self, a):
+        b = self.respond(a)
+        return b
