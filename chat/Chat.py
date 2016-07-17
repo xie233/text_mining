2b41cf60299a8808158460fd97b5946d189e36d2
xie <x1311178474@gmail.com>
2016-07-17 12:43:51 +0800

    Update Chat.py

--- chat/Chat.py
+++ chat/Chat.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import re
 import random
-from nltk import compat
+
 
 reflections = {
   "i am"       : "you are",
@@ -26,33 +26,33 @@ reflections = {
 class Chat(object):
     def __init__(self, pairs, reflections={}):
  
-        self._pairs = [(re.compile(x, re.IGNORECASE),y) for (x,y) in pairs]
-        self._reflections = reflections
-        self._regex = self._compile_reflections()
-    def _compile_reflections(self):
-        sorted_refl = sorted(self._reflections.keys(), key=len,
+        self.pairs = [(re.compile(x, re.IGNORECASE),y) for (x,y) in pairs]
+        self.reflections = reflections
+        self.regex = self.compile_reflections()
+    def compile_reflections(self):
+        sorted_refl = sorted(self.reflections.keys(), key=len,
                 reverse=True)
         return  re.compile(r"\b({0})\b".format("|".join(map(re.escape,
             sorted_refl))), re.IGNORECASE)
-    def _substitute(self, str):
+    def substitute(self, str):
         return self._regex.sub(lambda mo:
-                self._reflections[mo.string[mo.start():mo.end()]],
+                self.reflections[mo.string[mo.start():mo.end()]],
                     str.lower())
-    def _wildcards(self, response, match):
+    def wildcards(self, response, match):
         pos = response.find('%')
         while pos >= 0:
             num = int(response[pos+1:pos+2])
             response = response[:pos] + \
-                self._substitute(match.group(num)) + \
+                self.substitute(match.group(num)) + \
                 response[pos+2:]
             pos = response.find('%')
         return response
     def respond(self, str):
-        for (pattern, response) in self._pairs:
+        for (pattern, response) in self.pairs:
             match = pattern.match(str)
             if match:
                 resp = random.choice(response)  
-                resp = self._wildcards(resp, match) 
+                resp = self.wildcards(resp, match) 
                 if resp[-2:] == '?.': resp = resp[:-2] + '.'
                 if resp[-2:] == '??': resp = resp[:-2] + '?'
                 return resp
